2fae9949109c257b9a77172f041d221e32e55fac
Rangga857 <azharrangga28@gmail.com>
2023-04-03 10:16:02 +0700

    Add project files.

---
+++ ActivityStructure_150/ActivityStructure_150.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa
+{
+	string NIM;
+	string nama;
+	string alamat;
+	int umur;
+};
+
+int main() {
+	mahasiswa mhs1, mhs2;
+
+	mhs1.NIM = "20220140150";
+	mhs1.nama = "Wicaksono";
+	mhs1.alamat = "jogja";
+	mhs1.umur = 20;
+
+	cout << "Masukkan NIM : ";
+	cin >> mhs2.NIM;
+	cout << "Masukkan nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan alamat : ";
+	cin >> 
+
+
+}
