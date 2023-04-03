9f5d22ce6f48193627209e62ab0cbd264c7437e9
Rangga857 <azharrangga28@gmail.com>
2023-04-03 10:39:47 +0700

    membuat struct mahasiswa (1)

--- ActivityStructure_150/ActivityStructure_150.cpp
+++ ActivityStructure_150/ActivityStructure_150.cpp
@@ -26,6 +26,14 @@ int main() {
 	cout << "Masukkan umur : ";
 	cin >> mhs2.umur;
 
+	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nNama = " << mhs1.nama;
+	cout << "\nAlamat = " << mhs1.alamat;
+	cout << "\nUmur = " << mhs1.umur;
 
+	cout << "\n\nNIM = " << mhs2.NIM;
+	cout << "\nNama = " << mhs2.nama;
+	cout << "\nalamat = " << mhs2.alamat;
+	cout << "\numur = " << mhs2.umur;
 
 }
