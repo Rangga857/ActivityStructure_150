0156cb03675945da3af25429ff3cbf273bead1ea
Rangga857 <azharrangga28@gmail.com>
2023-04-03 11:00:32 +0700

    membuat cout atribut mahasiswa

--- Latihan2/Latihan2.cpp
+++ Latihan2/Latihan2.cpp
@@ -12,5 +12,28 @@ struct Mahasiswa
 	string NIM;
 	string nama;
 	AlamatDetail alamat;
-	int unsur;
+	int umur;
 };
+
+int main() {
+	Mahasiswa mhs;
+	
+	cout << "Masukkan NIM = ";
+	cin >> mhs.NIM;
+	cout << "Masukkan nama = ";
+	cin >> mhs.nama;
+	cout << "alamat = ";
+	cout << "\n\tMasukkan Desa = ";
+	cin >> mhs.alamat.desa;
+	cout << "\tMasukkan Kota = ";
+	cin >> mhs.alamat.kota;
+	cout << "Masukkan umur = ";
+	cin >> mhs.umur;
+
+	cout << "\n\nNIM = " << mhs.NIM;
+	cout << "\nnama = " << mhs.nama;
+	cout << "\nDesa = " << mhs.alamat.desa;
+	cout << "\nKota = " << mhs.alamat.kota;
+	cout << "\numur = " << mhs.umur;
+
+}
